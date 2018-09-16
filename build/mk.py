12ae4b3063fda4ddcd2b66f8d7edae4e0af36a24
Zachary Cotton <zacharycotton@berkeley.edu>
2018-09-16 01:13:32 -0700

    Better Build Setup for quickly adding/modifying keywords

--- build/mk.py
+++ build/mk.py
@@ -1,19 +1,37 @@
 import re
 
-VERSIONS = ["chez","scm"]
+VERSIONS = {"gscm":["-chez","-scm"],"README":[".md"]}
 DEST = "../"
-SOURCE = "gscm.scm"
+SOURCES = ["gscm","README"]
 
-for version in VERSIONS:
-    f = open(SOURCE,"r")
-    cont = f.read()
-    f.close()
-    repls = re.findall("{{([^}]*)}}",cont)
-    for repl in repls:
-        file = repl.replace("version",version)
-        g = open(file,"r")
-        cont = cont.replace("{{"+repl+"}}", g.read())
-        g.close()
-    h = open(DEST+"gscm-"+version,"w")
-    h.write(cont)
-    h.close()
+s_keywds = []
+gs_keywds = []
+args = []
+
+f = open("keyword_table.txt","r")
+for line in f.readlines()[2:]:
+    parts = line.split("|")
+    s_keywds.append("'"+parts[1].strip())
+    gs_keywds.append("#\\"+parts[2].strip())
+    args.append(parts[3].strip())
+f.close()
+
+open("gen/scheme_keywords","w").write(" ".join(s_keywds))
+open("gen/golf_scheme_keywords","w").write(" ".join(gs_keywds))
+open("gen/keyword_args","w").write(" ".join(args))
+
+
+for source in SOURCES:
+    for version in VERSIONS[source]:
+        f = open(source,"r")
+        cont = f.read()
+        f.close()
+        repls = re.findall("{{([^}]*)}}",cont)
+        for repl in repls:
+            file = repl.replace("version",version.replace("-",""))
+            g = open(file,"r")
+            cont = cont.replace("{{"+repl+"}}", g.read())
+            g.close()
+        h = open(DEST+source+version,"w")
+        h.write(cont)
+        h.close()
